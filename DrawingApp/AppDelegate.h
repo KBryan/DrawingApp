6676e28d8fd19bbe3a7fdbb5bef47e22c8357705
KBryan <kwame@3einteractive.com>
2015-03-27 13:14:35 -0400

    Initial Commit

---
+++ DrawingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingApp
+//
+//  Created by Kwame Bryan on 2015-03-27.
+//  Copyright (c) 2015 3e Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingApp
+//
+//  Created by Kwame Bryan on 2015-03-27.
+//  Copyright (c) 2015 3e Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
