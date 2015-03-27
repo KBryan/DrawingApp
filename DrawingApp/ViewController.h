c7c84a397a0aecb0d4826b9443daa78dd4ffced7
KBryan <kwame@3einteractive.com>
2015-03-27 13:43:48 -0400

    Added CoreGraphics to project

--- DrawingApp/ViewController.h
+++ DrawingApp/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (strong,nonatomic) UIBezierPath* path;
+
 
 @end
 
--- DrawingApp/ViewController.m
+++ DrawingApp/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+@import CoreGraphics;
 
 @interface ViewController ()
 
@@ -14,11 +15,57 @@
 
 @implementation ViewController
 
+
+-(void)awakeFromNib {
+    //create a mutable path
+    self.path = [[UIBezierPath alloc]init];
+    self.path.lineJoinStyle = kCGLineJoinRound;
+    self.path.lineCapStyle = kCGLineCapRound;
+    self.path.lineWidth = 5.0;
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     *  Create a mutable path
+     *
+     *  @return <#return value description#>
+     */
+
 }
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /**
+     *  get the starting point
+     *
+     *  @return return nothing
+     */
+    CGPoint point = [[touches anyObject] locationInView:self.view];
+    /**
+     *  move the path drawing cursor to the starting point
+     *
+     *  @return returns nothing
+     */
+    [self.path moveToPoint:point];
+}
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    // get the current point
+    CGPoint point = [[touches anyObject] locationInView:self.view];
+    // add a new line segment to our path
+    [self.path addLineToPoint:point];
+    // redraw the view
+    [self.view setNeedsDisplay];
+    NSLog(@"Screen Touched");
+}
+-(void)drawRect:(CGRect) rect {
+    // draw path
+    [[UIColor clearColor] setFill];
+    [[UIColor redColor] setStroke];
+    [self.path stroke];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
